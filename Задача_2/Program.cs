adc9eeff74cb27c6d7b7e8afa89cf66a76537747
Grigory <kononoff88@gmail.com>
2022-09-15 21:38:22 +0300

    123

---
+++ Задача_2/Program.cs
@@ -0,0 +1,21 @@
+﻿//1. Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.;
+//a = 25, b = 5 -> да; 
+//a = 2, b = 10 -> нет; 
+//a = 9, b = -3 -> да ;
+//a = -3 b = 9 -> нет;
+Console.Clear();
+
+Console.Write("Введите число1: ");
+int number1 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите число2: ");
+int number2 = int.Parse(Console.ReadLine());
+
+if (Math.Pow(number1,2) == number2)
+{
+              Console.WriteLine("а = " + number1 +  ", b = " + number2 + " -> да");
+} 
+else
+{
+              Console.WriteLine("а = " + number1 +  ", b = " + number2 + " -> нет");
+}
---
+++ Задача_3/Program.cs
@@ -0,0 +1,28 @@
+﻿//3. Напишите программу, которая будет выдавать название 
+//дня недели по заданному номеру.
+//3 -> Среда 
+//5 -> Пятница
+
+Console.WriteLine ("Введите день недели");
+int week = int.Parse(Console.ReadLine());
+switch (week)
+{
+    case 1:
+        Console.WriteLine ("Понедельник");
+        break;
+    case 2:
+       Console.WriteLine ("Вторник");
+        break;
+    case 3:
+        Console.WriteLine ("Среда");
+        break;
+    case 4:
+       Console.WriteLine ("Четверг");
+       break;
+    case 5:
+        Console.WriteLine ("Пятница");
+        break;
+    default:
+        Console.WriteLine ("Вы ввели число не соответсвтующее дню неделт");
+        break;
+}
