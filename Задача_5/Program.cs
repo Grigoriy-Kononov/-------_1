1b39a9a33446efb17bce51a2f3649f6fdf69734d
Grigory <kononoff88@gmail.com>
2022-09-15 22:05:52 +0300

    222

--- Задача_5/Program.cs
+++ Задача_5/Program.cs
@@ -4,3 +4,13 @@
 // 456 -> 6
 // 782 -> 2
 // 918 -> 8
+
+using System;
+
+Console.WriteLine("Введите трехзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int num_dec = num - (num/100)*100;
+int num_l = num_dec-(num_dec / 10) * 10;
+
+Console.WriteLine(num_l);
